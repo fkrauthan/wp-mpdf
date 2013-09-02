766b771fc4d67e12d1d7e2944a958a977bfdea2f
fkrauthan <fkrauthan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-02 18:30:49 +0000

    Updated mpdf to version 5.7.1 Added missing folders Prepared version 3.0.1
    git-svn-id: http://plugins.svn.wordpress.org/wp-mpdf/trunk@765882 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-mpdf.php
+++ wp-mpdf.php
@@ -3,7 +3,7 @@
 Plugin Name: wp-mpdf
 Plugin URI: http://fkrauthan.de/eng/projects/php/7-wp-mpdf
 Description: Print a wordpress page as PDF with optional Geshi Parsing.
-Version: 3.0
+Version: 3.0.1
 Author: Florian 'fkrauthan' Krauthan
 Author URI: http://fkrauthan.de
 
